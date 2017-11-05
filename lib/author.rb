5163ef44df6694cc1043e76cd16997204ddf7c50
Travis Petersen <travis.e.petersen@gmail.com>
2017-11-05 21:27:56 +0000

    automatically backed up by learn

---
+++ lib/author.rb
@@ -0,0 +1,26 @@
+class Author
+  attr_accessor :name, :posts
+  @@post_count
+
+  def initialize(name)
+    @name = name
+    @posts = []
+  end
+
+  def add_post(post)
+    @posts << post
+    @@post_count += 1
+    post.author = self
+  end
+
+  def add_post_by_title(title)
+    new_post = Post.new(title)
+    self.add_post(new_post)
+  end
+
+  def post_count
+    @@post_count
+  end
+
+end
+    
